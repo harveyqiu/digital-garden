cceade0f342867e9a9bc191e2c489e7390d944fd
Harvey Qiu <qiushimao@hotmail.com>
2022-08-11 17:53:34 +0800

    remove prisma

--- components/Library/MarkCard.js
+++ components/Library/MarkCard.js
@@ -3,10 +3,10 @@ export default function MarkCard({mark}) {
         <>
                 <div className="my-2 border-gray border-2 rounded-lg p-2">
                         <div className="p-3 text-2xl line-clamp-3 text-gray-1000 dark:text-gray-100">
-                                    {mark.title}
+                            {mark.title}
                         </div>
                         <div className="p-3">
-                                    {mark.link}
+                            {mark.link}
                         </div>
                 </div>
         </>
