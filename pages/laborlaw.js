ff8574eef7b2bf3b2f1a7873fe7ce018fc2bc017
Harvey Qiu <qiushimao@hotmail.com>
2022-08-11 18:23:00 +0800

    labor law

--- pages/laborlaw.js
+++ pages/laborlaw.js
@@ -1,36 +1,50 @@
 import Layout from "../components/Layout";
-import { Title1, Title2 } from "../components/Typography";
-import styled from "styled-components";
+import { useState } from "react";
+
 
 export default function LaborLaw() {
+    const [cancelParty, setCancelParty] = useState("")
+
     return (
       <Layout>
-        <HeaderContainer>
-          <Title1>劳动法</Title1>
-          <Title2>
+        <div>
+          <h1>劳动法</h1>
+          <h2>
               简明劳动法
-          </Title2>
-        </HeaderContainer>
-        <MainSection>
+          </h2>
+        </div>
+        <div>
+            <div>
+                1. 解除
+            </div>
+            <div className="flex flex-row ">
+                <div className="mx-auto basis-1/3 text-center">
+                    <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => {
+                        setCancelParty("协商解除")
+                    }}>
+                        协商解除
+                    </button>
+                </div>
+                <div className="mx-auto basis-1/3 text-center">
+                    <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"  onClick={() => {
+                        setCancelParty("劳动者单方解除")
+                    }}>
+                    劳动者单方解除
+                    </button>
+                </div>
+                <div className="mx-auto basis-1/3 text-center">
+                    <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"  onClick={() => {
+                        setCancelParty("用人单位单方解除")
+                    }}>
+                    用人单位单方解除
+                    </button>
+                </div>
+            </div>
+            <div>
+                {cancelParty}
+            </div>
 
-        </MainSection>
+        </div>
       </Layout>
     );
 }
-
-const HeaderContainer = styled.div`
-  margin: var(--space-xl) auto;
-  max-width: 800px;
-  @media (max-width: 768px) {
-    margin: var(--space-m) 0;
-    max-width: 100%;
-  }
-`;
-
-const MainSection = styled.section`
-  margin: var(--space-xl) auto;
-  max-width: 800px;
-  @media (max-width: 768px) {
-    max-width: 100%;
-  }
-`;
